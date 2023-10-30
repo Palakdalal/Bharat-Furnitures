44e31bb5afccd7c075f1062f8c8fcbf3ca6741fe
Palakdalal <palakdalal1@gmail.com>
2023-10-31 00:23:57 +0530

    login page formed

---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">BHARAT FURNITURES</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Product And Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Wood</a></li>
+            <li><a class="dropdown-item" href="#">Plastic</a></li>
+            <li><a class="dropdown-item" href="#">Mattress</a></li>
+            <li><a class="dropdown-item" href="#">Handloom Work</a></li>
+            <li><a class="dropdown-item" href="#">Interior Designing</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
